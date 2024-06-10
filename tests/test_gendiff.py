75e0b9879de5174536d2890d07d63db93321c665
Anna Kudriaeva <anna.kudriaeva@gmail.com>
2024-06-10 23:32:08 +0300

    add test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,6 +5,7 @@ from gendiff.formatters.stylish import format_stylish
 from gendiff.formatters.plain import format_plain
 from gendiff.formatters.json import format_json
 from gendiff.gendiff import find_differences
+from gendiff.parser import parse_yaml
 
 @pytest.fixture
 def file1_data():
@@ -15,6 +16,21 @@ def file1_data():
 def file2_data():
     with open('tests/fixtures/file2.json') as f:
         return json.load(f)
+
+@pytest.fixture
+def file1_data_yaml():
+    return parse_yaml('tests/fixtures/file1.yml')
+
+@pytest.fixture
+def file2_data_yaml():
+    return parse_yaml('tests/fixtures/file2.yml')
+
+
+def test_generate_diff_yaml(file1_data_yaml, file2_data_yaml):
+    diff = generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
+    expected_diff = find_differences(file1_data_yaml, file2_data_yaml)
+    assert diff == expected_diff
+
 
 def test_generate_diff(file1_data, file2_data):
     diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
